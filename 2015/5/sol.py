5ca6216bd450dd1c2aa9449047b91dcad8d56446
Teikn Smith <spencerteiknsmith@gmail.com>
2021-12-11 21:54:53 -0700

    Actually solve 2015/5

--- 2015/5/sol.py
+++ 2015/5/sol.py
@@ -1,6 +1,7 @@
+from typing import Counter
 import numpy as np
 from sys import stdin
-from collections import deque as deq
+from collections import deque
 from copy import copy
 import functools as fntls
 import itertools as ittls
@@ -13,13 +14,51 @@ import heapq as q
 e = enumerate
 INF = float('inf')
 
+def nice(l):
+    cs = Counter(l)
+    if sum(cs[k] for k in "aeiou") < 3:
+        return False
+    if any(bad in l for bad in "ab cd pq xy".split()):
+        return False
+    prev = l[0]
+    for c in l[1:]:
+        if c == prev:
+            return True
+        prev = c
+    return False
+
+def nice2(l):
+    pairs = set()
+    p = l[0]
+    hasrep = False
+    hasgap = False
+    for c, n in zip(l[1:], l[2:]):
+        if l == "zgsnvdmlfuplrubt":
+            print(p, c,n)
+        if (c,n) in pairs:
+            if hasgap:
+                return True
+            hasrep = True
+        if p == n:
+            if hasrep:
+                return True
+            hasgap = True
+        pairs.add((p,c))
+        p  = c
+
+    return False
 
 def solve():
+    print(len(list(l for l in lines() if nice2(l))))
     pass
 
 
 def lines():
     return stdin.read().strip().split('\n')
+
+
+def intgridin():
+    return [[int(c) for c in r] for r in lines()]
 
 
 def groups():
