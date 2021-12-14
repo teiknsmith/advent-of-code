a6afe40d6c506df58e0bae3336c14165610672fd
Teikn Smith <spencerteiknsmith@gmail.com>
2021-12-13 20:19:06 -0700

    Cleanup 2015/1

--- 2015/1/sol.py
+++ 2015/1/sol.py
@@ -1,10 +1,4 @@
 import sys
-from collections import deque as deq
-import functools
-import math
-import numpy as np
-import re
-e = enumerate
 
 floor = 0
 lines = [l for l in sys.stdin.readlines()]
@@ -15,15 +9,9 @@ for c in lines[0]:
         solb = i
         break
     i += 1
-    
 
 sola = floor
-
-
-
-
 
 print("Part 1: ", sola)
 
 print("Part 2: ", solb)
-
