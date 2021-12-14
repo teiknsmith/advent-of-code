4f555c5742a353b0e3525e9c147a8afa1606170a
Teikn Smith <spencerteiknsmith@gmail.com>
2021-12-13 20:47:59 -0700

    Cleanup 2020/3

--- 2020/3/sol.py
+++ 2020/3/sol.py
@@ -1,24 +1,18 @@
 import sys
 
-l = sys.stdin.read().split('\n')
-del l[-1]
-w = len(l[0])
-steps = [
-    (1, 1),
-    (3, 1),
-    (5, 1),
-    (7, 1),
-    (1, 2)
-]
-c = 1
+lines = sys.stdin.read().split('\n')
+del lines[-1]
+w = len(lines[0])
+steps = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+res = 1
 for step in steps:
-    j = 0;
-    count = 0;
-    for el in l[::step[1]]:
-        if el[j] == '#':
+    j = 0
+    count = 0
+    for row in lines[::step[1]]:
+        if row[j] == '#':
             count += 1
         j += step[0]
         j %= w
-    c *= count
+    res *= count
 
-print(c)
+print(res)
