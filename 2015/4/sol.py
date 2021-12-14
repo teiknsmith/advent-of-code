90bf6db58ce4143aabc4b76e8fbdca0e4fa61a2b
Teikn Smith <spencerteiknsmith@gmail.com>
2021-12-13 20:25:05 -0700

    Cleanup 2015/4

--- 2015/4/sol.py
+++ 2015/4/sol.py
@@ -1,72 +1,14 @@
-import numpy as np
-from sys import stdin
-from collections import deque as deq
-from copy import copy
-import functools as fntls
-import itertools as ittls
-import operator as op
-import math
-import re
-import string
-import heapq as q
 import hashlib
-
-e = enumerate
-INF = float('inf')
 
 
 def solve():
     o = input()
-    # print(hashlib.md5((o + str(609043)).encode('utf-8')).hexdigest())
-    # return
     for i in range(int(1e7)):
-        if hashlib.md5(
-            (o + str(i)).encode('utf-8')).hexdigest()[:6] == "000000":
+        to_hash = (o + str(i)).encode('utf-8')
+        front_of_hash = hashlib.md5(to_hash).hexdigest()[:6]
+        if front_of_hash == "000000":
             print(i)
             break
-    pass
-
-
-def lines():
-    return stdin.read().strip().split('\n')
-
-
-def groups():
-    return [g.split('\n') for g in stdin.read().strip().split('\n\n')]
-
-
-def btwn(v, l, h):
-    return (v > l and v < h) or (v < l and v > h)
-
-
-def btwne(v, l, h):
-    return (v >= l and v <= h) or (v <= l and v >= h)
-
-
-def btwni(v, l, h):
-    return v >= l and v < h
-
-
-def valids(ijl, hii, hij):
-    for i, j in ijl:
-        if (btwni(i, -INF if hii == INF else 0, hii)
-                and btwni(j, -INF if hii == INF else 0, hij)):
-            yield (i, j)
-
-
-def neigh4(i, j, hii=INF, hij=INF):
-    yield from valids([(i - 1, j), \
-                        (i + 1, j), \
-                        (i, j - 1), \
-                        (i, j + 1)], hii, hij)
-
-
-def neigh8(i, j, hii=INF, hij=INF):
-    yield from neigh4(i, j, hii, hij)
-    yield from valids([(i - 1, j - 1), \
-                        (i - 1, j + 1), \
-                        (i + 1, j - 1), \
-                        (i + 1, j + 1)], hii, hij)
 
 
 if __name__ == '__main__':
