aa44e3c725d2981aff0a89022f727d664b4cdd8d
Teikn Smith <spencerteiknsmith@gmail.com>
2021-12-19 21:48:50 -0700

    Re-solve 2021/17

--- 2021/17/sol.py
+++ 2021/17/sol.py
@@ -1,30 +1,95 @@
 import math
 import re
+from collections import defaultdict
+
+INF = float('inf')
 
 
+def vx_for_targ(t, x, is_moving=None):
+    if 2 * x >= (t**2 + t) if is_moving is None else is_moving:
+        return x / t + (t - 1) / 2
+    else:
+        return (math.sqrt(1 + 8 * x) - 1) / 2
 
-def lands(vx, vy, x1, x2, y1, y2):
-    x, y = 0, 0
-    while y >= 2 * y1 - 10:
-        if btwne(x, x1, x2) and btwne(y, y1, y2):
-            return True
-        x += vx
-        y += vy
-        vy -= 1
-        vx = max(vx - 1, 0)
-    return False
+
+def rect_area(x1, x2, y1, y2):
+    if x2 < x1 or y2 < y1:
+        return 0
+    return (x2 - x1 + 1) * (y2 - y1 + 1)
 
 
 def calc(x1, x2, y1, y2):
     # for completeness's sake, I copied part 1 from the old calc here
     print(((-y1 - 1) * -y1) / 2)
 
+    vx_for_left = math.ceil(vx_for_targ(None, x1, False))
+    vx_for_right = math.floor(vx_for_targ(None, x2, False))
+    vxs = defaultdict(lambda: (vx_for_left, vx_for_right))
+    for t in range(1, vx_for_left):
+        vxs[t] = (math.ceil(vx_for_targ(t, x1)),\
+                  math.floor(vx_for_targ(t, x2)))
+    for t in range(vx_for_left, vx_for_right + 1):
+        vxs[t] = (vx_for_left, math.floor(vx_for_targ(t, x2)))
+
+    # y is flipped bc it made more sense in my head
+    ymin = -y2
+    ymax = -y1
+    triangle_num = 0
+    prev_rect = [INF, INF, -INF, -INF]
+    tot = 0
+    for t in range(-2 * y1 + 1):
+        t += 1
+        triangle_num += t
+        min_vy = math.ceil((ymin - triangle_num) / t) + 1
+        max_vy = math.floor((ymax - triangle_num) / t) + 1
+        if max_vy < min_vy:
+            continue
+
+        # flipping y back for these. don't @ me
+        rect = [*vxs[t], -max_vy, -min_vy]
+        overlap = [prev_rect[0], rect[1], rect[2], prev_rect[3]]
+        tot += rect_area(*rect) - rect_area(*overlap)
+
+        prev_rect = rect
+    print(tot)
+
+
+def lands(vx, vy, x1, x2, y1, y2):
+    x, y = 0, 0
+    t = 0
+    ts = []
+    while y >= 2 * y1 - 10 and x <= 2 * x2 + 10:
+        if btwne(x, x1, x2) and btwne(y, y1, y2):
+            ts.append(t)
+        x += vx
+        y += vy
+        vy -= 1
+        vx = max(vx - 1, 0)
+        t += 1
+    return ts
+
+
+def oldcalc(x1, x2, y1, y2):
+    # for completeness's sake, I copied part 1 from the old calc here
+    print(((-y1 - 1) * -y1) / 2)
+
+    rs = defaultdict(lambda: [INF, -INF, INF, -INF])
+
     c = 0
     for vx in range(1, x2 + 1):
         for vy in range(y1, -y1):
-            if lands(vx, vy, x1, x2, y1, y2):
+            ts = lands(vx, vy, x1, x2, y1, y2)
+            if ts:
                 c += 1
+                for t in ts:
+                    rs[t][0] = min(rs[t][0], vx)
+                    rs[t][1] = max(rs[t][1], vx)
+                    rs[t][2] = min(rs[t][2], vy)
+                    rs[t][3] = max(rs[t][3], vy)
+    for t in sorted(rs):
+        print(t, rs[t])
     print(c)
+
 
 """
 # I'm keeping this here as a lesson to myself, and a possible future project.
@@ -75,6 +140,7 @@ def oldcalc(x1, x2, y1, y2):
     print(wacks)
     print(straight_ins)
 """
+
 
 def solve():
     x1, x2, y1, y2 = map(
